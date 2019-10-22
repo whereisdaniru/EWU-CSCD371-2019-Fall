a0fa0a726151c722d55ec3dbbcad9fa1b3307b30
Mark Michaelis <mark@IntelliTect.com>
2019-10-22 13:34:06 -0700

    Created assignment 4.

---
+++ Configuration/IConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Configuration
+{
+    public interface IConfig
+    {
+        public bool GetConfigValue(string name, string? value);
+
+        public bool SetConfigValue(string name, string? value);
+    }
+}
