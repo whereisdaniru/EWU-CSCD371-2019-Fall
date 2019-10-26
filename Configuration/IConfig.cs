e4c405fa6a0fe7b5b06c637bc3c494eb9c78b9e2
Mark Michaelis <mark@IntelliTect.com>
2019-10-26 15:51:45 -0700

    Changed IConfig.GetConfigValue's value parameter to be an out parameter.

--- Configuration/IConfig.cs
+++ Configuration/IConfig.cs
@@ -5,7 +5,7 @@ namespace Configuration
 {
     public interface IConfig
     {
-        public bool GetConfigValue(string name, string? value);
+        public bool GetConfigValue(string name, out string? value);
 
         public bool SetConfigValue(string name, string? value);
     }
