66b9b4f048783e54c75a9174fd7e16d671840550
Kevin Bost <kitokeboo@gmail.com>
2019-10-29 14:03:21 -0700

    Starting work

---
+++ Lecture1029/Lecture1029/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture1029
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
