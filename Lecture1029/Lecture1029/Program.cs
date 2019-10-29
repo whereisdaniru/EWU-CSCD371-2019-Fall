cd4018148f50e9f7f79019899ea8a9cfdc99d7c0
Kevin Bost <kitokeboo@gmail.com>
2019-10-29 16:20:18 -0700

    Operators and stuff

--- Lecture1029/Lecture1029/Program.cs
+++ Lecture1029/Lecture1029/Program.cs
@@ -1,55 +1,93 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Lecture1029
 {
     class Program
     {
-        private readonly object _Foo;
-
-        public Program()
-        {
-            _Foo = new object();
-        }
-
         static void Main(string[] args)
         {
-            (int, int) numbers = (1, 2);
-            Print(numbers); // (1, 2)
-            AddNumbers(numbers);
-            Print(numbers); // (1, 2)? (3, 2)?
+            var hashSet = new HashSet<Person>();
+            //var d = new Dictionary<int, string>();
+            //d.Add(0, "");
+            //d.Add(0, "Foo");
+            //d[0] = "Foo";
 
-            Pets pet = default;
+            var kevin1 = new Person("Kevin", DateTime.Today.AddDays(-1), ssn: "123456");
+            var kevin2 = new Person("Kevin", DateTime.Today.AddDays(-1), ssn: "123456");
+            var kevin3 = new Person("Kevin", DateTime.Today.AddDays(-1), ssn: "123456");
 
-            pet = Pets.Dog;
+            DateTime tomrrow = DateTime.Now + TimeSpan.FromDays(1);
+            kevin1 = null;
+            //Console.WriteLine($"Equals {kevin1.Equals(kevin2)}");
+            Console.WriteLine($"== {kevin1 == kevin2}");
 
-            Print(pet);
-        }
-
-        public static void AddNumbers((int a, int b) numbers)
-        {
-            //a += b;
-            //numbers.Item1 += numbers.Item2;
-
-             
+            hashSet.Add(kevin1);
+            hashSet.Add(kevin2);
+            hashSet.Add(kevin3);
+            Console.WriteLine(hashSet.Count);
+            //Print(kevin);
         }
 
         public static void Print(object foo) => Console.WriteLine(foo.ToString());
     }
 
-
-    [Flags]
-    public enum Pets
+    [DebuggerDisplay("{Name} {Dob}")]
+    public class Person : IEquatable<Person>
     {
-        Unknown = 0,
-        None = Unknown,
+        public string Name { get; }
 
-        FlyDog = Dog | Chicken,
+        public DateTime Dob { get; }
 
-        Dog = 0b_0000,
-        Cat = 0b_0001,
-        Bird = 0b_0010,
-        Chicken = 0b_0100
+        private string Ssn { get; }
+
+        public Person(string name, DateTime dob, string ssn = null)
+        {
+            Dob = dob;
+            Ssn = ssn;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public bool Equals([AllowNull] Person other)
+        {
+            if (ReferenceEquals(other, null)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return Name == other.Name &&
+                   Dob == other.Dob &&
+                   Ssn == other.Ssn;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Person);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name, Dob, Ssn).GetHashCode();
+            //int rv = Name.GetHashCode();
+            //rv = rv ^ Dob.GetHashCode() * 397;
+            //rv ^= Ssn?.GetHashCode() * 397 ?? 0;
+            //return rv;
+            //return Name.GetHashCode() + Dob.GetHashCode() + (Ssn?.GetHashCode() ?? 0);
+        }
+        
+        public static bool operator ==(Person a, Person b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (ReferenceEquals(a, null)) return false;
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Person a, Person b) => !(a == b);
+
+        public override string ToString()
+        {
+            return $"{Name} {Dob:d} {Ssn?[^4..]}";
+        }
+
     }
 }
