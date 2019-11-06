f378d3796dd86aae02fe891402a9ae07db1c0ede
Mark Michaelis <mark@IntelliTect.com>
2019-11-05 22:40:11 -0800

    Finished example from class.

---
+++ Configuration.Tests/MemoryConfigTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Configuration.Tests
+{
+    [TestClass]
+    public class MyTestClass
+    {
+        [TestMethod]
+        public void SetValue_GivenNewReferenceTypeValue_Success()
+        {
+            MemoryConfig config = new MemoryConfig();
+            string expected = "Inigo Montoya";
+            config.SetValue("name", expected!);
+            Assert.AreEqual<string>(
+                expected, config.GetValue<string>("name"));
+        }
+
+        [TestMethod]
+        public void SetValue_Given2NewValueTypeValue_Success()
+        {
+            MemoryConfig config = new MemoryConfig();
+            (string name, int value) = ("name", 42);
+            config.SetValue(name, value);
+            Assert.AreEqual<int>(
+                value, config.GetValue<int>(name));
+            (name, value) = ("forgive", 77);
+            config.SetValue(name, value);
+            Assert.AreEqual<int>(
+                value, config.GetValue<int>(name));
+        }
+
+        [TestMethod]
+        public void SetValue_GivenReferenceTyeNull_Success()
+        {
+            MemoryConfig config = new MemoryConfig();
+            (string name, string? value) = ("name", null);
+            config.SetValue(name, value);
+            Assert.AreEqual<string?>(
+                value, config.GetValue<string?>(name));
+        }
+
+        [TestMethod]
+        public void SetValue_GivenValueTypeNull_Success()
+        {
+            MemoryConfig config = new MemoryConfig();
+            (string name, int? value) = ("name", null);
+            config.SetValue(name, value);
+            // Remind class about AreEqual<T> on Thursday
+            Assert.AreEqual<int?>(
+                value, config.GetValue<int?>(name));
+        }
+
+
+        [TestMethod]
+        public void SetValue_GivenNewValueWithGenerics_Success()
+        {
+            // Refactor in class on Thursday
+        }
+
+
+        static MemoryConfig DataRowsConfig = new MemoryConfig();
+        [TestMethod]
+        [DataRow("age", 42)]
+        [DataRow("null", null)]
+        [DataRow("name", "Inigo Montoya")]
+        [DataRow("name", "Princess Buttercup")]
+        [DataRow("size", "large")]
+        [DataRow("size", 42)]
+        [DataRow("BigSize", 85)]
+        [DataRow("size", 42)]
+        public void SetAndGetValue_DataRows_Success(string name, object? value)
+        {
+            // Yuck!!  Discuss in class Thursday.
+            DataRowsConfig.SetValue(name, value);
+            Assert.AreEqual<object?>(
+                value, DataRowsConfig.GetValue<object?>(name));
+        }
+    }
+ 
+}
---
+++ Configuration/MemoryConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Configuration
+{
+    public class MemoryConfig
+    {
+        Dictionary<string, object?> InternalCollection { get; } =
+            new Dictionary<string, object?>();
+
+        Dictionary<Type, System.Collections.IDictionary> InternalValueTypeCollection =
+            new Dictionary<Type, System.Collections.IDictionary>();
+
+        public void SetValue<TValue>(string name, TValue value)
+        {
+            if(typeof(TValue).IsValueType)
+            {
+                System.Collections.IDictionary collection;
+                if (InternalValueTypeCollection.TryGetValue(typeof(TValue), 
+                    out collection))
+                {
+                    ((IDictionary<string, TValue>)collection)[name] = value;
+                }
+                else
+                {
+                    collection = new Dictionary<string, TValue>();
+                    collection.Add(name, value);
+                    InternalValueTypeCollection.Add(typeof(TValue), collection);
+                }
+            }
+            else
+            {
+                InternalCollection[name] = value;
+            }
+        }
+
+        public TValue GetValue<TValue>(string name)
+        {
+            if (typeof(TValue).IsValueType)
+            {
+                return ((IDictionary<string, TValue>)InternalValueTypeCollection[typeof(TValue)])[name];
+            }
+            else
+            {
+                // Discuss use of null forgive operator.
+                return (TValue)InternalCollection[name]!;
+            }
+        }
+    }
+}
